3aae9f91cb7d1035e9645d3f76a6afaa17f549f8
Vitaliy Kuzin <saynschuman@gmail.com>
2016-09-21 15:29:12 +0300

    test

--- html/1-2/js/main.js
+++ html/1-2/js/main.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
 
 	$('.navbar-toggle').click(function(){
 		$('.main').toggle();
+		$('.full_width_background').toggle();
+		$('.main_block_footer').toggle();
 		
 	});
 
