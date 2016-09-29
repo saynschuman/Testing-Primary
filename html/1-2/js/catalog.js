6886065df28d32afab23e351430a2052d3f50d7c
Vitaliy Kuzin <saynschuman@gmail.com>
2016-09-29 15:33:45 +0300

    menu

--- html/1-2/js/catalog.js
+++ html/1-2/js/catalog.js
@@ -6,13 +6,17 @@ $('.cl').click(function(){
 })
 
  $('.toggle_button').click(function(){
-  $('ul.mobile').slideToggle();
+  $('ul.mobile').slideToggle(200);
 });
 
  $('.cat_click').click(function(){
-  $('ul.sidebar').slideToggle();
+  $('ul.sidebar').slideToggle(200);
 });
 
  $('.show_more').click(function(){
- 	$('.more_inf').slideToggle();
+ 	$('.more_inf').slideToggle(200);
  });
+
+ $('.btn_filters_block').click(function(){
+ 	$('.filters_block').slideToggle(300);
+ })
